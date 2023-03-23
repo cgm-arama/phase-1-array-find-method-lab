11a92edf0a22a5c0f930ffa10c55ab1b31d62f9b
Eric <eric.arama@student.moringaschool.com>
2023-03-23 12:09:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+
+  
+  const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+  
+  const winYear = superbowlWin(record);
+  console.log(winYear); // "2015"
+   
